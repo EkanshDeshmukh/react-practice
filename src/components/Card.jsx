9b6c025a1f1390e9c1f6e843639cbce82f73932f
Ekansh-Deshmukh <yashudeshmukh25@email.com>
2024-03-21 23:57:18 +0530

    Understanding form handling and Usestate

---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Card({values,index,handleClick}) {
+  const {image,profession,added,name} = values                
+  
+  return (
+    <div className="mt-10">
+      <div className="w-64 h-64 bg-slate-100 flex justify-center items-center rounded-md">
+        <img src={image}  className="w-32 h-32 rounded-full" />
+        <div className="ml-4">
+          <h1 className="text-xl font-bold whitespace-nowrap">{name}</h1>
+          <p className="text-gray-600">{profession}</p>
+          <button onClick={() => handleClick(index)} className={`px-3 py-2 ${added === false ? "bg-blue-500" : "bg-green-500"} text-white rounded-md mt-10`}>
+     {added ? "Friends":"Connect"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar( {values}) {
+  return (
+    <>
+    <div className='w-full  px-4 py-3 flex items-center justify-between'>
+        <h3>Facebook</h3>
+        <div className='px-3 py-2 bg-blue-600 text-white rounded-lg font-semiBold'>
+            <h3>No. of Friends</h3>
+            <h4>{values.filter(item => item.added).length}</h4>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
